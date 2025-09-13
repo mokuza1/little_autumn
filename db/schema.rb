7e0f6270dda3990e6b7a4cd0702493f608b5644c
mokuza1e <karen239wo@outlook.jp>
2025-09-13 21:14:39 +0900

    マイグレーションファイル編集

--- db/schema.rb
+++ db/schema.rb
@@ -15,7 +15,7 @@ ActiveRecord::Schema[8.0].define(version: 2025_09_13_114818) do
   enable_extension "pg_catalog.plpgsql"
 
   create_table "posts", force: :cascade do |t|
-    t.text "content"
+    t.text "content", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
